0b2b483063c99b5e922b129fa506ff342b6da761
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-08-21 23:42:36 +0300

    Add example for internal and npm modules

---
+++ JavaScript/2-require.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const fs = require('fs');
+const events = require('node:events'); // > 16
+const ws = require('ws');
+const exp = require('./1-export.js');
+const { Entity, fn, collection } = exp;
+
+console.log(Object.keys(fs));
+console.log(Object.keys(events));
+console.log(Object.keys(ws));
+console.log(Object.keys(exp));
