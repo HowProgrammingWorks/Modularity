fc3e222f92845e96493e571c74f1537267edc9d2
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-08-22 15:39:35 +0300

    ES modules example

---
+++ JavaScript/8-export.mjs
@@ -0,0 +1,8 @@
+class Entity {}
+
+const fn = x => x;
+
+const collection = new Map();
+
+// module.exports = { Entity, fn, collection };
+export { Entity, fn, collection };
---
+++ JavaScript/9-import.mjs
@@ -0,0 +1,5 @@
+import { Entity, fn, collection } from './8-export.mjs';
+import m1 from './1-export.js';
+
+console.log({ Entity, fn, collection });
+console.log(m1);
