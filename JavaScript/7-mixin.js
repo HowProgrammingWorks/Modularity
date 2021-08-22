d09e353d22571600d52ed0f7ef04ce9a236fd2fa
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-08-22 15:39:07 +0300

    Mixin to modules and intercept

---
+++ JavaScript/7-mixin.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fs = require('fs');
+const { readFile } = fs;
+
+fs.readFile = (...args) => {
+  const path = args.shift();
+  const callback = args.pop();
+  const options = args.pop() || {};
+  console.log(`Intercepted call: fs.readFile('${path}')`);
+  return readFile(path, options, (err, data) => {
+    if (err) console.error(err);
+    else console.log(`Data received: ${data.length}`);
+    callback(err, data);
+  });
+};
+
+fs.readFile('./7-mixin.js', (err, data) => {
+  console.log({ err, data });
+});
