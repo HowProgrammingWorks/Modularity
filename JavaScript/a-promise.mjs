0f7f69d60b1c8adb609286f0042ea3e96e139a6d
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-08-22 23:47:37 +0300

    Use import as a functions

---
+++ JavaScript/a-promise.mjs
@@ -0,0 +1,9 @@
+const promise = import('events');
+console.log({ promise });
+
+promise.then((events) => {
+  console.log({ defaultMaxListeners: events.defaultMaxListeners });
+})
+
+const events = await import('events');
+console.log({ defaultMaxListeners: events.defaultMaxListeners });
