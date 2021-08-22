e67ac24e40954d1efe8a1580d0ff68c6c8d6ff94
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-08-22 15:38:33 +0300

    Require as singleton and global

---
+++ JavaScript/5-singleton.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const { collection } = require('./1-export.js');
+collection.set('key1', 'value1');
---
+++ JavaScript/6-global.js
@@ -0,0 +1,6 @@
+'use strict';
+
+require('./5-singleton.js');
+
+const { collection } = require('./1-export.js');
+console.log(collection);
