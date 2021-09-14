59164a7c05144b3f3b060837db38372ee0e32c30
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-09-14 17:04:34 +0300

    Fix code style

--- JavaScript/a-promise.mjs
+++ JavaScript/a-promise.mjs
@@ -3,7 +3,7 @@ console.log({ promise });
 
 promise.then((events) => {
   console.log({ defaultMaxListeners: events.defaultMaxListeners });
-})
+});
 
 const events = await import('events');
 console.log({ defaultMaxListeners: events.defaultMaxListeners });
