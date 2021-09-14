772e670ddee609ca2b820d47b74a29b480729f22
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-09-14 23:05:26 +0300

    Improve examples

--- JavaScript/4-cache.js
+++ JavaScript/4-cache.js
@@ -1,12 +1,13 @@
 'use strict';
 
-require('./1-export.js');
-
+const ex1 = require('./1-export.js');
 const modulePath = require.resolve('./1-export.js');
-console.log({ modulePath });
+console.log({ required: modulePath });
 console.log(require.cache[modulePath]);
 delete require.cache[modulePath];
-console.log(require.cache[modulePath]);
+console.log({ cached: require.cache[modulePath] });
+const ex2 = require('./1-export.js');
+console.log(ex1 === ex2);
 
 const ws = require('ws');
 const wsPath = require.resolve('ws');
--- JavaScript/c-require.mjs
+++ JavaScript/c-require.mjs
@@ -1,5 +1,8 @@
 import { createRequire } from 'module';
+console.log({ 'import.meta': import.meta });
 const require = createRequire(import.meta.url);
 
 const fs = require('fs');
 console.log(Object.keys(fs));
+
+console.log({ require });
